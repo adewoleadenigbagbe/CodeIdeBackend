92e68b5488048b1ca926756633cfbddd7236d80a
Adewole <wolecharles@gmail.com>
2024-09-22 20:46:56 +0100

    added more implementation

--- CodeIdeBackend/Models/TestRequest.cs
+++ CodeIdeBackend/Models/TestRequest.cs
@@ -2,6 +2,7 @@
 using System;
 using System.CodeDom.Compiler;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
 using System.Web;
@@ -29,7 +30,6 @@ namespace CodeIdeBackend.Models
             var provider = CodeDomProvider.CreateProvider("CSharp");
             var loParameters = new CompilerParameters();
 
-
             //Add reference library
             loParameters.ReferencedAssemblies.Add("System.dll");
 
@@ -37,20 +37,61 @@ namespace CodeIdeBackend.Models
             loParameters.GenerateInMemory = true;
 
             //compile the code
-            CompilerResults loCompiled = provider.CompileAssemblyFromSource(loParameters, request.Code);
+            var userCodeCompiled = provider.CompileAssemblyFromSource(loParameters, request.Code);
 
-            if(loCompiled.Errors.HasErrors)
+            if(userCodeCompiled.Errors.HasErrors)
             {
                 string lcErrorMsg = "";
 
-                lcErrorMsg = loCompiled.Errors.Count.ToString() + " Errors:";
-                for (int x = 0; x < loCompiled.Errors.Count; x++)
+                lcErrorMsg = userCodeCompiled.Errors.Count.ToString() + " Errors:";
+                for (int x = 0; x < userCodeCompiled.Errors.Count; x++)
                 {
-                    lcErrorMsg = lcErrorMsg + "\r\nLine: " + loCompiled.Errors[x].Line.ToString() + " - " +
-                                loCompiled.Errors[x].ErrorText;
+                    lcErrorMsg = lcErrorMsg + "\r\nLine: " + userCodeCompiled.Errors[x].Line.ToString() + " - " +
+                                userCodeCompiled.Errors[x].ErrorText;
                 }
                 throw new Exception(lcErrorMsg);
             }
+
+
+            //Add reference library
+            var loParameters2 = new CompilerParameters();
+
+            //Add reference library
+            loParameters2.ReferencedAssemblies.Add("System.dll");
+
+            loParameters2.GenerateInMemory = true;
+            var testCompiled = provider.CompileAssemblyFromSource(loParameters2, UnitTestCode.TestCode);
+
+
+            if (testCompiled.Errors.HasErrors)
+            {
+                string lcErrorMsg = "";
+
+                lcErrorMsg = testCompiled.Errors.Count.ToString() + " Errors:";
+                for (int x = 0; x < testCompiled.Errors.Count; x++)
+                {
+                    lcErrorMsg = lcErrorMsg + "\r\nLine: " + testCompiled.Errors[x].Line.ToString() + " - " +
+                                testCompiled.Errors[x].ErrorText;
+                }
+                throw new Exception(lcErrorMsg);
+            }
+
+            var process = new Process();
+            var startInfo = new ProcessStartInfo();
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+
+            //this going to be the unit test argument
+            startInfo.Arguments = "";
+
+            process.Start();
+
+            process.WaitForExit();
+
+            // Read the output of the process.
+            string output = process.StandardOutput.ReadToEnd();
+
+            Console.WriteLine(output);
 
             return new TestResponse();
         }
---
+++ CodeIdeBackend/Models/UnitTestCode.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeIdeBackend.Models
+{
+    public class UnitTestCode
+    {
+        public const string TestCode = @"
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LearningCSharpConcept;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningCSharpConcept.Tests
+{
+    [TestClass()]
+    public class FizzBuzzTests
+    {
+
+        [TestMethod()]
+        public void FizzBuzz_GetOutput_Method_Returns_Fizz_When_Number_Is_Divisible_By_Only_3()
+        {
+            var expected = ""Fizz"";
+
+            var actual = FizzBuzz.GetOutput(3);
+            Assert.AreEqual(actual, expected);
+
+            var actual1 = FizzBuzz.GetOutput(6);
+            Assert.AreEqual(actual1, expected);
+
+            var actual2 = FizzBuzz.GetOutput(9);
+            Assert.AreEqual(actual2, expected);
+        }
+
+        [TestMethod()]
+        public void FizzBuzz_GetOutput_Method_Returns_Buzz_When_Number_Is_Divisible_By_Only_5()
+        {
+            var expected = ""Buzz"";
+
+            var actual = FizzBuzz.GetOutput(5);
+            Assert.AreEqual(actual, expected);
+
+            var actual1 = FizzBuzz.GetOutput(10);
+            Assert.AreEqual(actual1, expected);
+
+            var actual2 = FizzBuzz.GetOutput(20);
+            Assert.AreEqual(actual2, expected);
+        }
+
+        [TestMethod()]
+        public void FizzBuzz_GetOutput_Method_Returns_FizzBuzz_When_Number_Is_Divisible_By_3_And_5()
+        {
+            var expected = ""FizzBuzz"";
+
+            var actual = FizzBuzz.GetOutput(15);
+            Assert.AreEqual(actual, expected);
+
+            var actual1 = FizzBuzz.GetOutput(30);
+            Assert.AreEqual(actual1, expected);
+
+            var actual2 = FizzBuzz.GetOutput(45);
+            Assert.AreEqual(actual2, expected);
+        }
+
+        [TestMethod()]
+        public void FizzBuzz_GetOutput_Method_Returns_Number_When_Number_Is_Not_Divisible_By_3_Or_5()
+        {
+            var expected = ""7"";
+            var actual = FizzBuzz.GetOutput(7);
+            Assert.AreEqual(actual, expected);
+
+            var expected1 = ""17"";
+            var actual2 = FizzBuzz.GetOutput(17);
+            Assert.AreEqual(actual2, expected1);
+
+
+            var expected2 = ""1"";
+            var actual3 = FizzBuzz.GetOutput(1);
+            Assert.AreEqual(actual3, expected2);
+        }
+    }
+}
+";
+    }
+}

8ca2ccd9cd57f45834a849c904de9170fe365dc2
Adewole <wolecharles@gmail.com>
2024-09-22 20:08:47 +0100

    second commit

--- CodeIdeBackend/Models/TestRequest.cs
+++ CodeIdeBackend/Models/TestRequest.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Microsoft.CSharp;
+using System;
+using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 
 namespace CodeIdeBackend.Models
@@ -23,6 +26,32 @@ namespace CodeIdeBackend.Models
     {
         public TestResponse Handle(TestRequest request)
         {
+            var provider = CodeDomProvider.CreateProvider("CSharp");
+            var loParameters = new CompilerParameters();
+
+
+            //Add reference library
+            loParameters.ReferencedAssemblies.Add("System.dll");
+
+            //Load the result assembly into memory
+            loParameters.GenerateInMemory = true;
+
+            //compile the code
+            CompilerResults loCompiled = provider.CompileAssemblyFromSource(loParameters, request.Code);
+
+            if(loCompiled.Errors.HasErrors)
+            {
+                string lcErrorMsg = "";
+
+                lcErrorMsg = loCompiled.Errors.Count.ToString() + " Errors:";
+                for (int x = 0; x < loCompiled.Errors.Count; x++)
+                {
+                    lcErrorMsg = lcErrorMsg + "\r\nLine: " + loCompiled.Errors[x].Line.ToString() + " - " +
+                                loCompiled.Errors[x].ErrorText;
+                }
+                throw new Exception(lcErrorMsg);
+            }
+
             return new TestResponse();
         }
     }

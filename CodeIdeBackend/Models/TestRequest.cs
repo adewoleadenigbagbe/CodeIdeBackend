5cee9b3c8b60d091331a4ef39d06ac3118a85d68
Adewole <wolecharles@gmail.com>
2024-09-27 02:31:17 +0100

    added random file name

--- CodeIdeBackend/Models/TestRequest.cs
+++ CodeIdeBackend/Models/TestRequest.cs
@@ -3,7 +3,9 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.CodeDom.Compiler;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Web;
@@ -26,6 +28,8 @@ namespace CodeIdeBackend.Models
 
     public class Handler
     {
+        private readonly string dllFolderPath = ConfigurationManager.AppSettings["dllFolderPath"];
+        private readonly string vsTestFile = ConfigurationManager.AppSettings["vsTestFile"];
         public string Handle(TestRequest request)
         {
             //check cache 
@@ -37,21 +41,23 @@ namespace CodeIdeBackend.Models
             var provider = CodeDomProvider.CreateProvider("CSharp");
 
             var loParameters = new CompilerParameters();
+
+            //Load the result assembly into memory
+            loParameters.GenerateInMemory = false;
+
             //Add reference library
             loParameters.ReferencedAssemblies.Add("System.dll");
             loParameters.ReferencedAssemblies.Add("System.Linq.dll");
             loParameters.ReferencedAssemblies.Add(typeof(Assert).Assembly.Location);
 
-            //Load the result assembly into memory
-            //loParameters.GenerateInMemory = true;
+            var outputAssembly = dllFolderPath +GetRandomFileName() +".dll";
+            loParameters.OutputAssembly = outputAssembly;
 
-            //compile the user code
+            //compile code
             var compilerResults = provider.CompileAssemblyFromSource(loParameters, request.Code, UnitTestCode.TestCode);
             if (compilerResults.Errors.HasErrors)
             {
-                string lcErrorMsg = "";
-
-                lcErrorMsg = compilerResults.Errors.Count.ToString() + " Errors:";
+                var lcErrorMsg = compilerResults.Errors.Count.ToString() + " Errors:";
                 for (int x = 0; x < compilerResults.Errors.Count; x++)
                 {
                     lcErrorMsg = lcErrorMsg + "\r\nLine: " + compilerResults.Errors[x].Line.ToString() + " - " +
@@ -65,7 +71,7 @@ namespace CodeIdeBackend.Models
             var processInfo = new ProcessStartInfo
             {
                 WindowStyle = ProcessWindowStyle.Maximized,
-                FileName = "vstest.console.exe",
+                FileName = vsTestFile,
                 RedirectStandardOutput = true,
                 RedirectStandardError = true,
                 UseShellExecute = false,
@@ -110,6 +116,20 @@ namespace CodeIdeBackend.Models
                 };
             }
             return output;
+        }
+
+        private static string GetRandomFileName()
+        {
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var stringChars = new char[8];
+            var random = new Random();
+
+            for (int i = 0; i < stringChars.Length; i++)
+            {
+                stringChars[i] = chars[random.Next(chars.Length)];
+            }
+
+            return new string(stringChars);
         }
     }
 }
